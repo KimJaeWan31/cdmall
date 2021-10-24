ed1b13586c50bb8cc90b8dabe44a9714f2e3cd96
jinbazone <jinbazone@10301287-9bc1-4641-81cb-8506f649292c>
2021-10-24 12:45:16 +0000

    사진 게시판 댓글 기능 에러 수정 by 설아

--- cdmalldraft1/src/main/java/com/demo/cdmall1/web/controller/mvc/CustomerCenterMvcController.java
+++ cdmalldraft1/src/main/java/com/demo/cdmall1/web/controller/mvc/CustomerCenterMvcController.java
@@ -1,18 +1,15 @@
 package com.demo.cdmall1.web.controller.mvc;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerCenterMvcController {
 	
-	@PreAuthorize("isAnonymous()")
 	@GetMapping("/customerCenter/faqList")
 	public void faqList() {
 	}
 	
-	//@PreAuthorize("isAnonymous()")
 	@GetMapping("/customerCenter/vocList")
 	public void voc() {
 	}
